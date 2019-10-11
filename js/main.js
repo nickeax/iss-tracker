0467314fa2427af7b42d847be8cbed76e8be83b6
nickeax <nickeax@gmail.com>
2019-10-11 12:36:25 +1100

    Add map

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 // Variables
-const baseAPI = "https://api.wheretheiss.at/v1/satellites/25544";
+const output = document.querySelector('#output');
+const interval = 1500;
 
 // Fetch data from API
-async function getData(apiURL) {
-  let data = await fetch(apiURL);
-  return data.json();
+async function getData() {
+  let data;  
+  const baseAPI = "https://api.wheretheiss.at/v1/satellites/25544";
+  data = await fetch(baseAPI);
+  let dataParsed = await data.json();
+  output.innerHTML = dataParsed.latitude; 
 }
+setInterval(getData, interval);
 
-console.log(getData(baseAPI));
